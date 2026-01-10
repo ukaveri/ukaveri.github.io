4696027c798d2d98a641d8a66bb4535caaedf8aa
ukaveri <kaveriupadrashta7@gmail.com>
2026-01-10 11:51:17 +0530

    modified

--- webapp1/script.js
+++ webapp1/script.js
@@ -1,11 +1,20 @@
 function dispMessage(){
-   // alert("Hello")   in java script // is used to comment the lines.
-//    a = 10
-//    b = 20
-//    c = a+b
-//    alert(c)
-    alert(document.getElementById("txtEmail").value);
-
-
-
+    // a=10
+    // b=20
+    // c=a+b
+    //alert("Hello")
+    // // alert(document.getElementById("txtEmail").value)
+    // lblMessage.innerHTML = document.getElementById("txtEmail").value
+    // lblMessage2.innerHTML = document.getElementById("txtPassword").value
+    let email = document.getElementById("txtEmail").value
+    let password = document.getElementById("txtPassword").value
+    if(email === "kaveriupadrashta7@gmail.com" && password === "kavs" )
+    {
+        // here if i take == it checks only value but if i use === it checks value with data type
+        lblMessage1.innerHTML = "welcome!!!! kaveriiii";
+    }
+    else
+    {
+        lblMessage1.innerHTML = "Accessed Denied registered for kaveri";
+    }
 }
