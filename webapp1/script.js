3d93551263e0fc6800ce59e1bd5a7f0f1c66e3cb
ukaveri <kaveriupadrashta7@gmail.com>
2026-01-10 11:03:27 +0530

    updated

---
+++ webapp1/script.js
@@ -0,0 +1,11 @@
+function dispMessage(){
+   // alert("Hello")   in java script // is used to comment the lines.
+//    a = 10
+//    b = 20
+//    c = a+b
+//    alert(c)
+    alert(document.getElementById("txtEmail").value);
+
+
+
+}
